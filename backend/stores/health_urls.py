f2c74397fab71f6a75ad1be2bfdff031d2fab3d9
Dan Brothers <brothersd@hotmail.com>
2026-03-24 18:06:31 -0600

    fix a docker crash loop

--- backend/stores/health_urls.py
+++ backend/stores/health_urls.py
@@ -2,6 +2,8 @@
 from django.urls import path, include
 from .views import health_check, ready_check, live_check
 
+app_name = 'health'
+
 urlpatterns = [
     path('ready/', ready_check, name='readiness-check'),
     path('live/', live_check, name='liveness-check'),
